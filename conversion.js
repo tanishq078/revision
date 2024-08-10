950ce1f62d9eef3b306d8167d945f596a68b0bd8
tanishq078 <stanishq69@gmail.com>
2024-08-11 02:01:50 +0530

    more revision

---
+++ conversion.js
@@ -0,0 +1,94 @@
+let score = "33abc";
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+
+// NaN -> Not a number (it is a number data type)
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+
+// "33ab" => NaN  not a number (type0f number)
+
+// true => 1; false => 0;
+
+let isLoggedIN = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIN);
+
+// console.log(booleanIsLoggedIn);
+
+// "" => false 
+// 1 => true
+// 0 => false
+// "tanishq" => true
+
+let someNumber = 33;
+
+// let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************OPERATIONS*********************
+
+let value = 3;
+
+let negvalue = -value;
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="tan";
+
+let str2 = "ishq";
+
+let str3 = str1+ str2;
+
+console.log(str3);
+
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+"2");//12
+console.log(1+2+"2");//32
+console.log("1"+2+2);//122
+
+
+console.log((3+4)*5%3); // 2
+
+console.log(5%3); //2
+
+console.log(+true); // 1
+
+console.log(+""); //0
+
+let num1, numm2, num3;
+
+num1 = numm2 = num3 = 1+1;
+
+let gameCounter = 100;
+
+gameCounter++;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
